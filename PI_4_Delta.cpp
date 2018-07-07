b75727b368810070e51a0387166e1b7441c9d4a9
Arturo Reyes Leal <gizmoh@localhost.localdomain>
2018-07-07 19:41:19 -0400

    added interval

--- PI_4_Delta.cpp
+++ PI_4_Delta.cpp
@@ -2,6 +2,7 @@
 #include <sdsl/int_vector.hpp>
 #include <sdsl/util.hpp>
 #include <math.h>
+#include <time.h>
 
 using namespace std;
 using namespace sdsl;
@@ -13,6 +14,7 @@ void testing(int X[], int Y[], int Z[], int largo){ //X arreglo de gaps; Y sampl
     int testing  = 0;
     int resultado = 0;
     int sampled = 0;
+    srand (time(NULL));
     testing  = rand()%largo;
     sampled = trunc(testing/sampling);
     resultado = Y[sampled];
@@ -50,9 +52,30 @@ int main (int argc, char** argv){// Recibe como argumento el largo del arreglo
         S[i] = X[i*sampling];
     }
 
-    for(int i=0; i < 25; i++){
+    /*for(int i=0; i < 25; i++){
         testing (Y,S,X, largo);
-    }
+    }*/
 
     cout << "gap mayor: " << mayor << endl;
+    cout << "calculo de distribuciones:" << endl;
+
+    int top = trunc(mayor *0.875);
+    int bot = trunc(mayor *0.125);
+    int contadorI = 0; 
+    int contadorO = 0;
+    double porcentajeI = 0;
+    double porcentajeO = 0;
+    cout << bot << " " << top << endl;
+    for(int i = 0; i < largo; i++){
+        if(bot < Y[i] and top > Y[i]){
+            contadorI++;
+        }
+        else{
+            contadorO++;
+        }
+    }
+    porcentajeI = double(contadorI)/double(largo);
+    porcentajeO = double(contadorO)/double(largo);
+    cout << "porcentaje en el intervalo: " << porcentajeI << " " << contadorI;
+    cout << " porcentaje fuera del intervalo: " << porcentajeO<< " " << contadorO << endl;
 }
