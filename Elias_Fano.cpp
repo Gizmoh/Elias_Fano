418bb9acef0e6f4de0074e0af4f357c2c9fb9b41
Arturo Reyes Leal <gizmoh@localhost.localdomain>
2018-07-07 18:30:46 -0400

    Finished gap Coding

--- Elias_Fano.cpp
+++ Elias_Fano.cpp
@@ -10,6 +10,20 @@ using namespace sdsl;
 #define INC 128
 #define sampling 64
 
+void testing(int X[], int Y[], int Z[], int largo){ //X arreglo de gaps; Y sampling, Z arreglo original
+    int testing  = 0;
+    int resultado = 0;
+    int sampled = 0;
+    testing  = rand()%largo;
+    sampled = trunc(testing/sampling);
+    resultado = Y[sampled];
+    //cout << Y[sampled] << " "<< testing << " " << sampled << " " << sampled*sampling << endl;
+    for(int i = sampled*sampling;i < testing; i++){
+        resultado = resultado + X[i+1];
+    }
+    cout << testing << " " << resultado << " " << Z[testing] << endl;
+}
+
 
 int main (int argc, char** argv){// Recibe como argumento el largo del arreglo
     int largo = 0;
@@ -32,5 +46,8 @@ int main (int argc, char** argv){// Recibe como argumento el largo del arreglo
         S[i] = X[i*sampling];
     }
 
-    
+    for(int i=0; i < 25; i++){
+        testing (Y,S,X, largo);
+    }
+
 }
