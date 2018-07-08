979003400365c6deb8fba78dc4b06e8892958818
Arturo Reyes Leal <gizmoh@localhost.localdomain>
2018-07-07 20:31:26 -0400

    Added bit_vector_li

--- PI_4_Delta.cpp
+++ PI_4_Delta.cpp
@@ -9,13 +9,16 @@ using namespace sdsl;
 
 #define INC 128
 #define sampling 64
+#define largoARR 256
+typedef bit_vector::size_type size_type;
 
-void testing(int X[], int Y[], int Z[], int largo){ //X arreglo de gaps; Y sampling, Z arreglo original
+void testing(int X[], int Y[], int Z[]){ //X arreglo de gaps; Y sampling, Z arreglo original
     int testing  = 0;
     int resultado = 0;
     int sampled = 0;
+    int size = 0;
     srand (time(NULL));
-    testing  = rand()%largo;
+    testing  = rand()%largoARR;
     sampled = trunc(testing/sampling);
     resultado = Y[sampled];
     //cout << Y[sampled] << " "<< testing << " " << sampled << " " << sampled*sampling << endl;
@@ -26,34 +29,35 @@ void testing(int X[], int Y[], int Z[], int largo){ //X arreglo de gaps; Y sampl
 }
 
 
-int main (int argc, char** argv){// Recibe como argumento el largo del arreglo
-    int largo = 0;
+int main (int argc, char** argv){// Recibe como argumento el largoARR del arreglo
     double r = 0;
-    largo = atoi(argv[1]);
     int mayor = 0;
-    int X [largo]={};
-    int Y [largo]={};
-    int S [largo/sampling] = {};
+    int X [largoARR]={};
+    int Y [largoARR]={};
+    int S [largoARR/sampling] = {};
+    bit_vector_il<largoARR> Ex;
+    bit_vector_il<largoARR>::rank_1_type rankEx;
+    bit_vector_il<largoARR>::select_1_type selEx;
 
-    for(int i = 1; i< largo; i++){//Genero el arreglo no decreciente
+    for(int i = 1; i< largoARR; i++){//Genero el arreglo no decreciente
         r = rand()%INC;
         X[i]=X[i-1]+r;
     }
     Y[0] = X[0];
     mayor = X[0];
-    for(int i=1;i<largo;i++){//Genero el arreglo de gaps
+    for(int i=1;i<largoARR;i++){//Genero el arreglo de gaps
         Y[i]=X[i]-X[i-1];
         if (mayor  < Y[i]){
             mayor = Y[i];
         }
     }
     S[1]=Y[1];
-    for(int i = 1; i < round(largo/sampling);i++){//Genero el sampling
+    for(int i = 1; i < round(largoARR/sampling);i++){//Genero el sampling
         S[i] = X[i*sampling];
     }
 
     /*for(int i=0; i < 25; i++){
-        testing (Y,S,X, largo);
+        testing (Y,S,X);
     }*/
 
     cout << "gap mayor: " << mayor << endl;
@@ -66,7 +70,7 @@ int main (int argc, char** argv){// Recibe como argumento el largo del arreglo
     double porcentajeI = 0;
     double porcentajeO = 0;
     cout << bot << " " << top << endl;
-    for(int i = 0; i < largo; i++){
+    for(int i = 0; i < largoARR; i++){
         if(bot < Y[i] and top > Y[i]){
             contadorI++;
         }
@@ -74,8 +78,8 @@ int main (int argc, char** argv){// Recibe como argumento el largo del arreglo
             contadorO++;
         }
     }
-    porcentajeI = double(contadorI)/double(largo);
-    porcentajeO = double(contadorO)/double(largo);
+    porcentajeI = double(contadorI)/double(largoARR);
+    porcentajeO = double(contadorO)/double(largoARR);
     cout << "porcentaje en el intervalo: " << porcentajeI << " " << contadorI;
     cout << " porcentaje fuera del intervalo: " << porcentajeO<< " " << contadorO << endl;
 }
