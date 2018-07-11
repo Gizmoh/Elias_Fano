3c91073c0ed4433cf255fa7532286da57ae0931d
Arturo Reyes Leal <gizmoh@localhost.localdomain>
2018-07-11 18:40:22 -0400

    Trouble with the ulongs

--- PI_4_Delta.cpp
+++ PI_4_Delta.cpp
@@ -14,6 +14,39 @@ using namespace cds;
 #define largoARR 256
 typedef bit_vector::size_type size_type;
 
+void storeSamples(ulong *Samples, int X[],uint BitA){
+    int i,j;
+    for (i=j=0; i< round(largoARR/sampling); i++,j+=BitA){
+        setNum64(Samples,j,BitA,X[i]);
+    }
+}
+void Storage(int X[],ulong *Gaps, ulong *Excep, bit_vector Ex,int Mayor,int A,int contadorI, int contadorO){
+    int i,j,k,BitsA,BitsB;
+    BitsA = (1+int(log2(Mayor))); //bitsA almacena los bits necesarios para almacenar el Gap mas grande.
+    BitsB = (1+int(log2(Mayor-A))); //bitsB almacena los bits necesarios para almacenar el Gap mas grande, menos A.
+
+    Gaps = new ulong[contadorI*BitsA];//mmmmmmmmmmmm
+    Excep = new ulong[contadorO*BitsB];//needs more cowbell
+
+    for (i = 1,j,k=0;i < largoARR; i++){
+        if(i%(sampling+1)==0){
+            continue;
+        }
+        if(Ex[i]==0){
+            setNum64(Gaps,j,BitsA,(X[i]-A));
+            j+=BitsA;
+            cout << "Numero real: " << X[i] << endl;
+            cout <<"Numero almacenado: " << getNum64(Gaps,j, BitsA) << endl;
+        }
+        if(Ex[i]==1){
+            setNum64(Excep,k,BitsB,(X[i]));
+            k+=BitsB;
+            cout << "Numero real: " << X[i] << endl;
+            cout <<"Numero almacenado: " << getNum64(Excep,j, BitsB) << endl;
+        }
+    }
+}
+
 void testing(int X[], int Y[], int Z[]){ //X arreglo de gaps; Y sampling, Z arreglo original
     int testing  = 0;
     int resultado = 0;
@@ -43,7 +76,7 @@ int main (int argc, char** argv){// Recibe como argumento el largoARR del arregl
     bit_vector::select_1_type Ex_Sel1(&Ex);
     bit_vector::select_0_type Ex_Sel0(&Ex);
 
-    for(int i = 1; i< largoARR; i++){//Genero el arreglo no decreciente
+    for(int i = 0; i< largoARR; i++){//Genero el arreglo no decreciente
         r = rand()%INC;
         X[i]=X[i-1]+r;
     }
@@ -64,10 +97,16 @@ int main (int argc, char** argv){// Recibe como argumento el largoARR del arregl
         testing (Y,S,X);
     }*/
 
+    uint BitMayor = (1+int(log2(mayor)));
+    uint BitSamplingMayor = (1+int(log2(S[largoARR/sampling-1])));
+
     cout << "Gap mayor: " << mayor << endl;
-    cout << "Bits necesarios: " << (1+int(log2(mayor))) << endl;
+    cout << "Bits necesarios: " << BitMayor << endl;
     cout << "Sampling mayor: " << (S[largoARR/sampling-1]) << endl;
-    cout << "calculo de distribuciones:" << endl;
+    cout << "Bits necesarios: " << BitSamplingMayor << endl;
+    cout << "Calculo de distribuciones:" << endl;
+    cout << "Se necesitan " << (BitSamplingMayor * round(largoARR/sampling)) << " bits para almacenar el sampling" << endl;
+
 
     int top = trunc(mayor *0.95);
     int bot = trunc(mayor *0.05);
@@ -86,6 +125,11 @@ int main (int argc, char** argv){// Recibe como argumento el largoARR del arregl
             Ex[i] = 1;
         }
     }
+    Samples = new ulong[S[largoARR/sampling-1]];
+    storeSamples(Samples,X, BitSamplingMayor);
+    Storage(X,Gaps,Excep,Ex,mayor,top,contadorI,contadorO);
+
+
     porcentajeI = double(contadorI)/double(largoARR);
     porcentajeO = double(contadorO)/double(largoARR);
     cout << "Porcentaje en el intervalo: " << porcentajeI << " " << contadorI;
